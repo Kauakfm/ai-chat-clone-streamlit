fae72487bf9817b33b2ebbe872e825e1736c24b6
KauaNdevs <kaua.martins@ndevs.com.br>
2025-03-08 15:11:34 -0300

    feat(streamlit): add initial implementation of ChatGPT Marrom chatbot with Streamlit integration

---
+++ README.md
@@ -0,0 +1,33 @@
+🚀 ChatGPT Marrom – Assistente Virtual com Streamlit e OpenAI
+Este é um chatbot desenvolvido com Streamlit e a API da OpenAI, capaz de interagir com usuários e manter um histórico de mensagens.
+
+🛠️ Tecnologias Utilizadas
+Streamlit – Para criar a interface web interativa.
+OpenAI API – Para processar as respostas do chatbot.
+
+📌 Como Executar o Projeto
+
+Clone este repositório
+git clone https://github.com/kauakfm/chatgpt-marrom.git
+cd chatgpt-marrom
+
+Instale as dependências
+pip install -r requirements.txt
+
+Configure sua chave de API da OpenAI
+No arquivo .streamlit/secrets.toml, adicione:
+OPENAI_API_KEY="sua-chave-aqui"
+
+Execute o chatbot
+streamlit run streamlitGPT.py
+
+📂 Estrutura do Projeto
+📁 AI-CHAT-CLONE
+│── app.py                # Código principal do chatbot  
+│── .streamlit/secrets.toml # Arquivo para armazenar a chave da OpenAI  
+└── README.md             # Documentação do projeto  
+
+💡 Contribuição
+Sinta-se à vontade para abrir issues ou enviar pull requests com melhorias!
+
+📩 Contato: www.linkedin.com/in/kauakfm
---
+++ streamlitGPT.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from openai import OpenAI
+
+st.title("ChatGPT Of Brown")
+
+client = OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
+
+if "openai_model" not in st.session_state:
+  st.session_state['openai_model'] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+  st.session_state['messages'] = []
+
+for message in st.session_state.messages:
+  with st.chat_message(message['role']):
+    st.markdown(message['content'])
+
+if prompt := st.chat_input("Whats is up?"):
+  intrunctions = """Você é um secretário de uma clinica médica. Vocês atendem várias especialidades médicas.
+  Interaja com o usuário e quando ele quiser marcar uma consulta pergunte na ordem. 
+  1 Qual o plano de saúde que ele possui. 2. Melhor data e horário para a consulta. Se o plano de saúde for o Notredame, informe
+  ao usuário que não entendemos esse plano."""
+  
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  with st.chat_message("user"):
+    st.markdown(prompt)
+  
+  with st.chat_message("assistant"):
+    stream = client.chat.completions.create(
+      model= st.session_state['openai_model'],
+      messages = [
+        {"role": "user", "content": prompt},
+        {"role": "system", "content": intrunctions}
+      ],
+      stream= True
+    )
+    response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
